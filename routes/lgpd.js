7b8ecb7a389c886c9fa01545dcf38445cff00b76
Admin Everson <admin.everson@fibracem.local>
2023-10-20 14:50:55 -0300

    primeiro commit

--- routes/lgpd.js
+++ routes/lgpd.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path')
 
-const {home, novoDocumento, salvarPdf, visualizarPdf, enviarArquivo} = require('../controller/lgpd.js');
+const {home, novoDocumento, salvarPdf, visualizarPdf, enviarArquivo, newuser, registernewuser, saveRegisterNewUser} = require('../controller/lgpd.js');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -21,5 +21,7 @@ router.get("/novo-documento", novoDocumento);
 router.post("/ler-pdf", upload.array('arquivos'), salvarPdf);
 router.get("/visualizar/:id", visualizarPdf);
 router.get("/arquivo/:filename", enviarArquivo);
-
+router.get("/novo-usuario", newuser);
+router.get("/cadastro-novo-usuario", registernewuser);
+router.post("/cadastro-novo-usuario", saveRegisterNewUser);
 module.exports = router;
