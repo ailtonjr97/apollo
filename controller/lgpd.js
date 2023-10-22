02c3e3b519b8f33236256a03e750f8b7e46bb81e
Ailton de Souza Junior <ailton.souza97@hotmail.com>
2023-10-21 22:54:59 -0300

    Feita criptografia e renderizacao

--- controller/lgpd.js
+++ controller/lgpd.js
@@ -1,11 +1,20 @@
 const dbFiles = require('../db/lgpd');
+const files = require('../db/files')
+const fs = require('fs');
+const crypto = require('crypto');
 const path = require('path');
+const dotenv = require("dotenv");
+dotenv.config();
+
+const algorithm = 'aes-256-ctr';
+let key = process.env.FILESKEY;
+key = crypto.createHash('sha256').update(String(key)).digest('base64').substring(0, 32);
 
 const home = async(req, res)=>{
     try {
         res.render('lgpd/home', {
-            files: await dbFiles.showFiles(),
-            contagem: await dbFiles.countFiles()
+            files: await files.showFiles(),
+            contagem: await files.countFiles()
         });
     } catch (error) {
         console.log(error);
@@ -24,9 +33,16 @@ const novoDocumento = async (req, res)=>{
 
 const salvarPdf = async (req, res)=>{
     try {
+        const encrypt = (buffer) => {
+            const iv = crypto.randomBytes(16);
+            const cipher = crypto.createCipheriv(algorithm, key, iv);
+            const result = Buffer.concat([iv, cipher.update(buffer), cipher.final()]);
+            return result;
+        };
 
-        req.files.forEach(file => {
-            dbFiles.insertFiles(file.fieldname, file.originalname, file.encoding, file.mimetype, file.destination, file.filename, file.path, file.size)
+        req.files.forEach(async (file) => {
+            let criptografado = encrypt(file.buffer);
+            await files.insertFiles(criptografado, file.originalname, file.mimetype, file.size, file.fieldname, file.encoding)
         });
 
         res.redirect('/lgpd')
@@ -38,8 +54,8 @@ const salvarPdf = async (req, res)=>{
 
 const visualizarPdf = async(req, res)=>{
     try {
-        const consulta = await dbFiles.selectFile(req.params.id)
-        res.send(`<embed src="/lgpd/arquivo/${consulta[0].filename}" style="width: 100%; height: 700px"/>`);
+        const consulta = await files.visualizaFile(req.params.id)
+        res.send(`<embed src="/lgpd/arquivo/${consulta[0].id}#toolbar=0&navpanes=0&scrollbar=0"" style="width: 100%; height: 700px"/>`);
     } catch (error) {
         console.log(error);
         res.render('error');
@@ -48,7 +64,31 @@ const visualizarPdf = async(req, res)=>{
 
 const enviarArquivo = async(req, res) =>{
     try {
-        res.sendFile(path.join(__dirname, `../storage/${req.params.filename}`))
+        const decrypt = (encrypted) => {
+            // Get the iv: the first 16 bytes
+            const iv = encrypted.slice(0, 16);
+            // Get the rest
+            encrypted = encrypted.slice(16);
+            // Create a decipher
+            const decipher = crypto.createDecipheriv(algorithm, key, iv);
+            // Actually decrypt it
+            const result = Buffer.concat([decipher.update(encrypted), decipher.final()]);
+            return result;
+         };
+
+        const resultado = await files.selectFile(req.params.id)
+        const decrypted = decrypt(resultado[0].file);
+
+        const nomeArq = Date.now() + '-' + resultado[0].originalname
+
+        fs.writeFileSync(nomeArq, decrypted)
+
+        res.sendFile(path.join(__dirname, `../${nomeArq}`))
+
+        setTimeout(()=>{
+            fs.unlinkSync(nomeArq)
+        }, 60000)
+        
     } catch (error) {
         console.log(error);
         res.render('error');
--- routes/lgpd.js
+++ routes/lgpd.js
@@ -1,26 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const multer = require('multer');
+const fs = require('fs');
 const path = require('path')
 
 const {home, novoDocumento, salvarPdf, visualizarPdf, enviarArquivo, newuser, registernewuser, saveRegisterNewUser} = require('../controller/lgpd.js');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'storage/')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname.replace(path.extname(file.originalname), '') + '---' + Date.now() + path.extname(file.originalname))
-    }
-  })
-  
-  const upload = multer({ storage: storage })
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 router.get("/", home);
 router.get("/novo-documento", novoDocumento);
 router.post("/ler-pdf", upload.array('arquivos'), salvarPdf);
 router.get("/visualizar/:id", visualizarPdf);
-router.get("/arquivo/:filename", enviarArquivo);
+router.get("/arquivo/:id", enviarArquivo);
 router.get("/novo-usuario", newuser);
 router.get("/cadastro-novo-usuario", registernewuser);
 router.post("/cadastro-novo-usuario", saveRegisterNewUser);
